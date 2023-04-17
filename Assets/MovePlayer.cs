afd0838d8a1b85448a6d8c295da238f86876df5d
Micka09 <mickaelsouloumiac9@gmail.com>
2023-04-17 17:08:44 -0400

    Déplacement du joueur + Saut
    #1

--- Assets/MovePlayer.cs
+++ Assets/MovePlayer.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
+    public float moveSpeed;
+    public float jumpForce;
+    public Rigidbody2D body;
+    private Vector3 velocity = Vector3.zero;
+
+    public bool isJumping;
+    public bool isGrounded;
+
+    public Transform groundRight;
+    public Transform groundLeft;
+    
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics2D.OverlapArea(groundLeft.position, groundRight.position);
+
+        float horizontalmouvement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            isJumping = true;
+        }
+
+        PlayerMovement(horizontalmouvement);
         
+    }
+    void PlayerMovement(float horizontalmouvement)
+    {
+        Vector3 targetVelocity = new Vector3(horizontalmouvement, body.velocity.y);
+        body.velocity = Vector3.SmoothDamp(body.velocity, targetVelocity, ref velocity, .05f);
+        
+        if(isJumping == true) {
+
+            body.AddForce(new Vector2 (0f, jumpForce));
+            isJumping= false;
+        }
+
     }
 }
